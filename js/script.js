a37810dbdb2b526bbbc88a71975b55007494628a
Dalbvas <52789866+Dalbvas@users.noreply.github.com>
2019-08-27 14:50:21 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ function Mymensaje(){
     alert("funciono correctamente");
 }
 console.log("Este es un mensaje en consola");
+var button=document.createElement("button");
+button.innerHTML="touchme";
+var body=document.getElementsByTagName("footer")[0];
+body.appendChild(button);
+button.addEventListener("click",function()
+{
+    document.write(10+11);
+});
